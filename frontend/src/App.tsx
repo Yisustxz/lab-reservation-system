9b60260aca60c1978b35a04f342dce4f2258b19b
feredev <feredev2@gmail.com>
2025-07-08 20:33:57 -0400

    refactor: update import statements and format JSX in App component for improved readability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import ComputersPage from './pages/ComputersPage'
-import LabsPage from './pages/LabsPage'
-import ReservationsPage from './pages/ReservationsPage'
-import UsersPage from './pages/UsersPage'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import ComputersPage from "./pages/ComputersPage";
+import LabsPage from "./pages/LabsPage";
+import ReservationsPage from "./pages/ReservationsPage";
+import UsersPage from "./pages/UsersPage";
 
 export default function App() {
   return (
@@ -12,14 +12,19 @@ export default function App() {
         <Navbar />
         <div className="container mx-auto p-4">
           <Routes>
-            <Route path="/" element={<h1 className="text-3xl font-bold">Bienvenido a LabReserve</h1>} />
-            <Route path="/labs" element={<LabsPage />} />       
+            <Route
+              path="/"
+              element={
+                <h1 className="text-3xl font-bold">Bienvenido a LabReserve</h1>
+              }
+            />
+            <Route path="/labs" element={<LabsPage />} />
             <Route path="/computers" element={<ComputersPage />} />
             <Route path="/reservations" element={<ReservationsPage />} />
-            <Route path="/users" element={<UsersPage />} /> 
+            <Route path="/users" element={<UsersPage />} />
           </Routes>
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
